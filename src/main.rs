0d5fafacf004303589b36000522239b6ca74f608
skenn <skenn@google.com>
2026-07-20 16:57:04 +0000

    feat: add JjRepo to read files and directories from any jj commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// TODO: dead_code only exists for now since we haven't implemented main() yet
+
+#[allow(dead_code)]
+mod file;
+#[allow(dead_code)]
+mod jj_repo;
+
 fn main() {
     println!("Hello, world!");
 }
