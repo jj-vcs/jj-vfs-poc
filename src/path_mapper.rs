20219be59f31ff084074e973cd535521316745b0
skenn <skenn@google.com>
2026-07-31 13:18:45 +0000

    refactor: move file.rs content over to path_mapper.rs and rename File to DirectoryEntry

--- src/path_mapper.rs
+++ src/path_mapper.rs
@@ -4,9 +4,28 @@ use std::pin::Pin;
 use async_trait::async_trait;
 use futures::AsyncRead;
 use futures::Stream;
+use ustr::Ustr;
 
-use crate::file::File;
 use crate::jj_error::JjError;
+
+pub enum FileType {
+    File,
+    Directory,
+}
+
+pub struct DirectoryEntry {
+    pub name: Ustr,
+    pub file_type: FileType,
+}
+
+impl DirectoryEntry {
+    pub fn new(name: &str, file_type: FileType) -> Self {
+        Self {
+            name: Ustr::from(name),
+            file_type,
+        }
+    }
+}
 
 /// This trait represents a file in our virtual file system. This can either be
 /// a normal file you can read from or for example a directory, in which case
@@ -20,7 +39,7 @@ use crate::jj_error::JjError;
 pub trait VirtualFile: Send + Sync {
     async fn read(&self) -> Result<Pin<Box<dyn AsyncRead + Send>>, JjError>;
     async fn size(&self) -> Result<u64, JjError>;
-    async fn list<'a>(&'a self) -> Result<Box<dyn Stream<Item = File> + 'a>, JjError>;
+    async fn list<'a>(&'a self) -> Result<Box<dyn Stream<Item = DirectoryEntry> + 'a>, JjError>;
 }
 
 /// This trait represents the VFS mountpoint structure by mapping a given
