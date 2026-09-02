73823960a02b30b72ea630e8d7b8ec96caca3b4c
skenn <skenn@google.com>
2026-09-02 20:37:52 +0000

    feat: implement WritableCommitTreeFile::write

--- src/writable_commit_tree_file.rs
+++ src/writable_commit_tree_file.rs
@@ -4,6 +4,9 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use futures::AsyncRead;
+use futures::AsyncReadExt as _;
+use jj_lib::backend::CopyId;
+use jj_lib::backend::FileId;
 use jj_lib::backend::TreeValue;
 use jj_lib::merge::Merge;
 use jj_lib::merged_tree_builder::MergedTreeBuilder;
@@ -46,7 +49,6 @@ impl WritableCommitTreeFile {
         ReadonlyCommitTreeFile::new(&self.repo, wc_commit_id, self.path.clone()).await
     }
 
-    #[allow(dead_code)] // TODO: this will be used in the future for create, write and delete function
     async fn update_file(
         repo: Arc<ReadonlyRepo>,
         workspace_name: WorkspaceNameBuf,
@@ -150,12 +152,126 @@ impl VirtualFile for WritableCommitTreeFile {
     }
 
     #[tracing::instrument(skip(self))]
-    async fn write(&self, _offset: u64, _data: &[u8]) -> JjResult<u32> {
-        todo!()
+    async fn write(&self, offset: u64, data: &[u8]) -> JjResult<u32> {
+        let path = RepoPathBuf::from_relative_path(&self.path).map_err(|_| JjError::InvalidPath)?;
+
+        let wc_commit_id = self
+            .repo
+            .view()
+            .get_wc_commit_id(&self.workspace_name)
+            .cloned()
+            .ok_or(JjError::NotFound)?;
+        let commit = self.repo.store().get_commit_async(&wc_commit_id).await?;
+
+        let current_value = commit.tree().path_value(&path).await?;
+        let resolved_val = current_value.as_resolved().ok_or(JjError::NotAFile)?;
+        let (file_id, executable) = match resolved_val {
+            Some(TreeValue::File { id, executable, .. }) => (id, *executable),
+            Some(TreeValue::Tree(_)) => return Err(JjError::NotAFile),
+            Some(TreeValue::Symlink(_)) => return Err(JjError::NotAFile),
+            None => return Err(JjError::NotFound),
+            _ => return Err(JjError::NotAFile),
+        };
+
+        let mut reader = self.repo.store().read_file(&path, file_id).await?;
+        let mut content = Vec::new();
+        reader.read_to_end(&mut content).await?;
+
+        let offset = usize::try_from(offset).map_err(|_| {
+            JjError::IO(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Offset too large",
+            ))
+        })?;
+        if offset + data.len() > content.len() {
+            content.resize(offset + data.len(), 0);
+        }
+        content[offset..offset + data.len()].copy_from_slice(data);
+
+        let new_file_id: FileId = self.repo.store().write_file(&path, &mut &content[..]).await?;
+
+        let tree_value = TreeValue::File {
+            id: new_file_id,
+            executable,
+            copy_id: CopyId::placeholder(),
+        };
+
+        let bytes_written = data.len() as u32;
+
+        Self::update_file(
+            self.repo.clone(),
+            self.workspace_name.clone(),
+            path,
+            Some(tree_value),
+            "Update file content".to_string(),
+        )
+        .await?;
+
+        Ok(bytes_written)
     }
 
     #[tracing::instrument(skip(self))]
     async fn delete(&self) -> JjResult<()> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+    use crate::test_helpers::setup_test_repo;
+    use crate::virtual_file::VirtualFile;
+
+    #[tokio::test]
+    async fn test_write_file() {
+        let (_temp_dir, repo, _commit_id) = setup_test_repo().await;
+        let commit_tree = WritableCommitTreeFile::new(
+            repo.clone(),
+            WorkspaceNameBuf::from("default"),
+            PathBuf::from("file1.txt"),
+        );
+
+        let bytes_written = commit_tree.write(6, b"updated content").await.unwrap();
+        assert_eq!(bytes_written, 15);
+
+        let reloaded_repo = repo.reload_at_head().await.unwrap();
+        let new_commit_tree = WritableCommitTreeFile::new(
+            reloaded_repo,
+            WorkspaceNameBuf::from("default"),
+            PathBuf::from("file1.txt"),
+        );
+
+        let mut reader = new_commit_tree.read().await.unwrap();
+        let mut content = Vec::new();
+        reader.read_to_end(&mut content).await.unwrap();
+        assert_eq!(content, b"hello updated content");
+    }
+
+    #[tokio::test]
+    async fn test_write_file_not_found() {
+        let (_temp_dir, repo, _commit_id) = setup_test_repo().await;
+        let commit_tree = WritableCommitTreeFile::new(
+            repo,
+            WorkspaceNameBuf::from("default"),
+            PathBuf::from("nonexistent.txt"),
+        );
+
+        let res = commit_tree.write(0, b"data").await;
+        assert!(matches!(res, Err(JjError::NotFound)));
+    }
+
+    #[tokio::test]
+    async fn test_write_file_not_a_file() {
+        let (_temp_dir, repo, _commit_id) = setup_test_repo().await;
+        let commit_tree = WritableCommitTreeFile::new(
+            repo,
+            WorkspaceNameBuf::from("default"),
+            PathBuf::from("dir"),
+        );
+
+        let res = commit_tree.write(0, b"data").await;
+        assert!(matches!(res, Err(JjError::NotAFile)));
     }
 }
